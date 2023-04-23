dff4c0f1226cdda53514ecd820fab5a7735e94bf
KalilahClarke <kalilah.clarke@gmail.com>
2023-04-23 13:05:07 -0400

    Firebase update

--- front-end/src/Components/Dashboard/Filter/Achievements/Chart.js
+++ front-end/src/Components/Dashboard/Filter/Achievements/Chart.js
@@ -14,8 +14,8 @@ function Chart({data}) {
      
      <ResponsiveCalendar
         data={data}
-        from={`${year}-02-01`}
-        to={`${year}-12-30`}
+        from={`${year}-01-30`}
+        to={`${year}-12-01`}
         emptyColor="#eeeeee"
         colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
         margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
--- front-end/src/Components/Dashboard/Filter/Achievements/PieChart.js
+++ front-end/src/Components/Dashboard/Filter/Achievements/PieChart.js
@@ -85,40 +85,11 @@ function PieChart({completedData}) {
             
                 {
                     match: {
-                        id: 'c'
+                        id: 'erlang'
                     },
                     id: 'dots'
                 },
-                {
-                    match: {
-                        id: 'go'
-                    },
-                    id: 'dots'
-                },
-                {
-                    match: {
-                        id: 'python'
-                    },
-                    id: 'dots'
-                },
-                {
-                    match: {
-                        id: 'scala'
-                    },
-                    id: 'lines'
-                },
-                {
-                    match: {
-                        id: 'lisp'
-                    },
-                    id: 'lines'
-                },
-                {
-                    match: {
-                        id: 'elixir'
-                    },
-                    id: 'lines'
-                },
+               
                 {
                     match: {
                         id: 'javascript'
